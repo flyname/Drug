24628c7f2494470f8e8548fb121e625c5fce1dbd
flyname <1339477161@qq.com>
2017-12-07 18:51:02 +0800

    Signed-off-by: flyname <1339477161@qq.com>

--- app/Http/Controllers/Admin/IndexController.php
+++ app/Http/Controllers/Admin/IndexController.php
@@ -14,7 +14,7 @@ class IndexController extends Controller
      */
     public function index()
     {
-        return view('Admin/Index/list');
+        return view('Admin/Index/index');
     }
 
 }
